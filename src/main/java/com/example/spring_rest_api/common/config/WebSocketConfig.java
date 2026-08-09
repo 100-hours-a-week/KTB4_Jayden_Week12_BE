53b432784dc98af576ac38f6119113d5c78f974b
JIHYUNG KIM <martinjhkim7493@gmail.com>
2026-08-09 17:58:41 +0900

    chore: 운영환경 웹소켓 CORS 수정

--- src/main/java/com/example/spring_rest_api/common/config/WebSocketConfig.java
+++ src/main/java/com/example/spring_rest_api/common/config/WebSocketConfig.java
@@ -24,6 +24,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.addEndpoint("/ws-chat")
                 .setAllowedOriginPatterns(
                         "http://localhost:8080",
+                        "http://127.0.0.1:5500",
+                        "http://localhost:5500",
                         "http://localhost:5173"
                 );
     }
